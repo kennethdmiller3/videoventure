730c62123f1cd024284771f2e644b503ce301fbb
Ken Miller <Ken Miller@44a3119c-8d4d-0410-be80-6906e8c8b632>
2008-01-13 01:12:54 +0000

    Code changes: * Added Gunner template, loader, and initializer * Changed Gunner simulation to trace owner's path * Added "update angle" and "update position" parameters to Link
    Level changes:
    * Replaced enemy Carrier guns with three missile launchers
    * Added Gradius-style "multiple" option

--- Source/Gunner.cpp
+++ Source/Gunner.cpp
@@ -1,16 +1,94 @@
 #include "StdAfx.h"
 #include "Gunner.h"
 #include "Entity.h"
+#include "Link.h"
 
 namespace Database
 {
+	Typed<GunnerTemplate> gunnertemplate(0xe4c32aec /* "gunnertemplate" */);
 	Typed<Gunner *> gunner(0xe063cbaa /* "gunner" */);
+
+	namespace Loader
+	{
+		class GunnerLoader
+		{
+		public:
+			GunnerLoader()
+			{
+				AddConfigure(0xe063cbaa /* "gunner" */, Entry(this, &GunnerLoader::Configure));
+			}
+
+			void Configure(unsigned int aId, const TiXmlElement *element)
+			{
+				GunnerTemplate &gunner = Database::gunnertemplate.Open(aId);
+				gunner.Configure(element);
+				Database::gunnertemplate.Close(aId);
+			}
+		}
+		gunnerloader;
+	}
+
+	namespace Initializer
+	{
+		class GunnerInitializer
+		{
+		public:
+			GunnerInitializer()
+			{
+				AddActivate(0xe4c32aec /* "gunnertemplate" */, Entry(this, &GunnerInitializer::Activate));
+				AddDeactivate(0xe4c32aec /* "gunnertemplate" */, Entry(this, &GunnerInitializer::Deactivate));
+			}
+
+			void Activate(unsigned int aId)
+			{
+				const GunnerTemplate &gunnertemplate = Database::gunnertemplate.Get(aId);
+				Gunner *gunner = new Gunner(gunnertemplate, aId);
+				Database::gunner.Put(aId, gunner);
+			}
+
+			void Deactivate(unsigned int aId)
+			{
+				if (Gunner *gunner = Database::gunner.Get(aId))
+				{
+					delete gunner;
+					Database::gunner.Delete(aId);
+				}
+			}
+		}
+		gunnerinitializer;
+	}
 }
 
+
+// Gunner Template Constructor
+GunnerTemplate::GunnerTemplate(void)
+: mFollowLength(32)
+{
+}
+
+// Gunner Template Destructor
+GunnerTemplate::~GunnerTemplate(void)
+{
+}
+
+// Gunner Template Configure
+bool GunnerTemplate::Configure(const TiXmlElement *element)
+{
+	if (Hash(element->Value()) != 0xe063cbaa /* "gunner" */)
+		return false;
+
+	element->QueryFloatAttribute("follow", &mFollowLength);
+	return true;
+}
+
+
 // Gunner Constructor
-Gunner::Gunner(unsigned int aId)
+Gunner::Gunner(const GunnerTemplate &aTemplate, unsigned int aId)
 : Simulatable(aId)
 {
+	Entity *entity = Database::entity.Get(id);
+	mTrackPos.push_back(entity->GetPosition());
+	mTrackLength = 0.0f;
 }
 
 // Gunner Destructor
@@ -18,41 +96,66 @@ Gunner::~Gunner(void)
 {
 }
 
-// configure
-bool Gunner::Configure(const TiXmlElement *element)
-{
-	if (Hash(element->Value()) != 0xe063cbaa /* "gunner" */)
-		return false;
-
-	// process child elements
-	for (const TiXmlElement *child = element->FirstChildElement(); child != NULL; child = child->NextSiblingElement())
-	{
-		const char *label = child->Value();
-		switch (Hash(label))
-		{
-		case 0xf5674cd4 /* "owner" */:
-			{
-				unsigned int owner = Hash(child->Attribute("name"));
-				Database::owner.Put(id, Database::owner.Get(owner));
-			}
-			break;
-		}
-	}
-
-	return true;
-}
-
 // Gunner Simulate
 void Gunner::Simulate(float aStep)
 {
 	// get the owner
-	unsigned int aOwnerId = Database::owner.Get(id);
+	unsigned int aOwnerId = Database::backlink.Get(id);
+
+	// get the owner entity
+	Entity *owner = Database::entity.Get(aOwnerId);
 
 	// if the owner does not exist...
-	if (!Database::entity.Find(aOwnerId))
+	if (!owner)
 	{
 		// self-destruct
 		Database::Delete(id);
 		return;
 	}
+
+	// gunner template
+	const GunnerTemplate &gunner = Database::gunnertemplate.Get(id);
+
+	// get owner movement
+	float lastsegment = owner->GetPosition().Dist(mTrackPos.back());
+	if (lastsegment > 0)
+	{
+		// accumulate movement distance
+		mTrackLength += lastsegment;
+
+		// while there is excess track length...
+		while (mTrackLength > gunner.mFollowLength)
+		{
+			// get the excess length
+			float excess = mTrackLength - gunner.mFollowLength;
+
+			// get the first segment length
+			float firstsegment = mTrackPos[1].Dist(mTrackPos[0]);
+
+			// if the segment is longer than the excess...
+			if (firstsegment > excess)
+			{
+				// shorten the segment
+				mTrackPos[0] += excess / firstsegment * (mTrackPos[1] - mTrackPos[0]);
+				mTrackLength -= excess;
+				break;
+			}
+			else
+			{
+				// remove the segment
+				mTrackLength -= firstsegment;
+				mTrackPos.pop_front();
+			}
+		}
+
+		// add new position
+		mTrackPos.push_back(owner->GetPosition());
+	}
+
+	// move to new position
+	Entity *entity = Database::entity.Get(id);
+	entity->Step();
+	entity->SetPosition(mTrackPos.front());
+	entity->SetAngle(owner->GetAngle());
+	entity->SetVelocity(owner->GetVelocity());	// <-- HACK!
 }
--- Source/Gunner.h
+++ Source/Gunner.h
@@ -2,19 +2,34 @@
 
 #include "Simulatable.h"
 
+// gunner template
+class GunnerTemplate
+{
+public:
+	float mFollowLength;
+
+public:
+	GunnerTemplate(void);
+	~GunnerTemplate(void);
+
+	// configure
+	bool Configure(const TiXmlElement *element);
+};
+
 // gunner actor
 class Gunner : 
 	public Simulatable
 {
+protected:
+	std::deque<Vector2> mTrackPos;
+	float mTrackLength;
+
 public:
 	// constructor
-	Gunner(unsigned int aId = 0);
+	Gunner(const GunnerTemplate &aTemplate, unsigned int aId);
 
 	// destructor
 	~Gunner(void);
-
-	// configure
-	virtual bool Configure(const TiXmlElement *element);
 
 	// simulate
 	virtual void Simulate(float aStep);
