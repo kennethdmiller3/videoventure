2ddc82bfd96dd4ab6f1471d5eb8387e1ecb78453
kennethdmiller3 <kennethdmiller3@44a3119c-8d4d-0410-be80-6906e8c8b632>
2009-11-15 01:12:30 +0000

    Code Changes: * Added shared single-byte Hash function * Fixed shape inheritance (for the most part) * Added support for "edge chain" shape

--- trunk/Source/Core/Hash.h
+++ trunk/Source/Core/Hash.h
@@ -3,15 +3,18 @@
 #include <ctype.h>
 
 // fnv-1a hash
+inline unsigned int Hash(const unsigned char byte, unsigned int hash = 2166136261u)
+{
+	hash ^= byte;
+	hash *= 16777619u;
+	return hash;
+}
 inline unsigned int Hash(const void *data, size_t len, unsigned int hash = 2166136261u)
 {
-	const unsigned char *d = static_cast<const unsigned char *>(data);
-	const unsigned char *e = d + len;
+	const unsigned char * d = static_cast<const unsigned char *>(data);
+	const unsigned char * const e = d + len;
 	while (d < e)
-	{
-		hash ^= *d++;
-		hash *= 16777619u;
-	}
+		hash = Hash(*d++, hash);
 	return hash;
 }
 inline unsigned int Hash(const char *string, unsigned int hash = 2166136261u)
@@ -19,10 +22,7 @@ inline unsigned int Hash(const char *string, unsigned int hash = 2166136261u)
 	if (string == 0)
 		return 0;
 	for (const char *s = string; *s != 0; ++s)
-	{
-		hash ^= static_cast<unsigned char>(tolower(*s));
-		hash *= 16777619u;
-	}
+		hash = Hash(unsigned char(tolower(*s)), hash);
 	return hash;
 }
 
