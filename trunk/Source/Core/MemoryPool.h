403ab8f8fb5b33c5de999a8c964c5d5a2c31849e
kennethdmiller3 <kennethdmiller3@44a3119c-8d4d-0410-be80-6906e8c8b632>
2011-01-02 22:38:51 +0000

    * Added new methods to MemoryPool - Added a default constructor - Moved pool parameter setup from constructor to Setup - Moved initial chunk allocation from constructor to Init - Moved final chunk deallocation from destructor to Cleanup - Added methods to get pool parameters

--- trunk/Source/Core/MemoryPool.h
+++ trunk/Source/Core/MemoryPool.h
@@ -7,6 +7,9 @@ private:
 	// size of blocks in the pool
 	size_t mSize;
 
+	// number of blocks to add on start
+	size_t mStart;
+
 	// number of blocks to add when out of blocks
 	size_t mGrow;
 
@@ -21,15 +24,45 @@ private:
 	void Grow(size_t aCount);
 
 public:
+	// default constructor
+	GAME_API MemoryPool(void);
+
 	// consructor
-	GAME_API MemoryPool(size_t aSize, size_t aCount = 256, size_t aGrow = 16);
+	GAME_API MemoryPool(size_t aSize, size_t aStart = 256, size_t aGrow = 16);
 
 	// destructor
 	GAME_API ~MemoryPool();
+
+	// setup
+	GAME_API void Setup(size_t aSize, size_t aStart, size_t aGrow);
+
+	// initialize
+	GAME_API void Init(void);
+
+	// cleanup
+	GAME_API void Clean(void);
 
 	// allocate a block
 	GAME_API void * Alloc(void);
 
 	// free a block
 	GAME_API void Free(void * aPtr);
+
+	// get block size
+	size_t GetSize(void) const
+	{
+		return mSize;
+	}
+
+	// get start block count
+	size_t GetStart(void) const
+	{
+		return mStart;
+	}
+
+	// get grow block count
+	size_t GetGrow(void) const
+	{
+		return mGrow;
+	}
 };
