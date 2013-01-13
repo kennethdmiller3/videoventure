2dd3aa5737a66a4448a35a285427349bbe9dda99
kennethdmiller3@gmail.com <kennethdmiller3@gmail.com@44a3119c-8d4d-0410-be80-6906e8c8b632>
2013-01-13 08:42:30 +0000

    * Refactored ExpressionOperator - Moved shareable portions to Utility so other code can use it * Created UtilitySIMD as a companion to Utility - SIMD arithmetic operators - SIMD specializations of Utility functions - Most of it was extracted from ExpressionOperatorSIMD - This makes working with SIMD much nicer :) * Eliminated ExpressionOperatorSIMD - Utility SIMD specializations make ExpressionOperator templates sufficient

--- Source/Core/Utility.h
+++ Source/Core/Utility.h
@@ -10,8 +10,33 @@ template <typename O, typename I> inline O Cast(I i)
 	return cast.o;
 }
 
-// fast reciprocal square root
-inline float InvSqrt(float x)
+// zero value
+template<typename T> inline T Zero()
+{
+	return T(0);
+}
+
+// extend a float
+template<typename T> inline T Extend(const float x)
+{
+	return T(x);
+}
+
+// reciprocal
+inline float Rcp(const float x)
+{
+	return 1.0f / x;
+}
+
+// square root
+// (for consistency with SIMD)
+inline float Sqrt(const float x)
+{
+	return sqrtf(x);
+}
+
+// reciprocal square root
+inline float InvSqrt(const float x)
 {
 	float xhalf = 0.5f*x;
 	union { float f; int i; } floatint;
@@ -22,19 +47,74 @@ inline float InvSqrt(float x)
 }
 
 // linear interpolation
-template<typename T> inline T Lerp(T v0, T v1, float s)
+template<typename T> inline T Lerp(const T v0, const T v1, const float s)
 {
 	return v0 + (v1 - v0) * s;
 }
 
-// value clamp
-template<typename T> inline T Clamp(T v, T min, T max)
+// absolute value
+inline float Abs(const float x)
+{
+	return fabsf(x);
+}
+
+// minimum of two values
+template<typename T> inline T Min(const T a, const T b)
+{
+	return a < b ? a : b;
+};
+
+// maximum of two values
+template<typename T> inline T Max(const T a, const T b)
+{
+	return a > b ? a : b;
+};
+
+// clamp value between zero and one
+template<typename T> inline T Clamp01(const T v)
+{
+	if (v < 0)
+		return 0;
+	if (v > 1)
+		return 1;
+	return v;
+}
+
+// clamp value between minimum and maximum
+template<typename T> inline T Clamp(const T v, const T min, const T max)
 {
 	if (v < min)
 		return min;
 	if (v > max)
 		return max;
 	return v;
+}
+
+// sign of a value
+template<typename T> inline T Sign(const T v)
+{
+	return (v == 0) ? T(0) : ((v > 0) ? T(1) : T(-1));
+}
+
+// fractional part of a value
+inline float Frac(const float v)
+{
+	return v - floorf(v);
+}
+
+// step function
+inline float Step(const float e, const float v)
+{
+	return v < e ? 0.0f : 1.0f;
+}
+
+// smooth-step function
+inline float SmoothStep(const float e0, const float e1, const float v)
+{
+	if (v <= e0) return 0.0f;
+	if (v >= e1) return 1.0f;
+	const float t = (v - e0) / (e1 - e0);
+	return t * t * (3 - 2 * t);
 }
 
 // rectangle template
@@ -45,7 +125,10 @@ template<typename T> struct Rect
 	T w;
 	T h;
 };
-
 #ifndef SDL_arraysize
 #define SDL_arraysize(array)	(sizeof(array)/sizeof(array[0]))
 #endif
+
+// SIMD utilities
+// including specializations of the above functions for SIMD
+#include "UtilitySIMD.h"
